d20017e8f0dc9e60d7bd131785783a7d34318cb5
Marie Färnström <fernstrm@hotmail.com>
2026-09-10 12:55:06 +0200

    Added panel component ChoiceSummary

---
+++ src/ui/panels/ChoiceSummary.tsx
@@ -0,0 +1,23 @@
+export function ChoiceSummary() {
+    const selection = {
+        body: "Sedan",
+        color: "Midnight blue",
+        rim: '18" Alloy',
+        tire: "All-season",
+    }
+
+    const sum = 1250000
+
+    return (
+        <div className="w-48 h-48 justify-self-end self-end text-primary-text">
+            <h2>CHOICE SUMMARY</h2>
+            <p><span className="text-secondary-text">Body</span> {selection.body}</p>
+            <p><span className="text-secondary-text">Color</span> {selection.color}</p>
+            <p><span className="text-secondary-text">Rim</span> {selection.rim}</p>
+            <p><span className="text-secondary-text">Tire</span> {selection.tire}</p>
+
+            <p>{sum}</p>
+            <button>Place order</button>
+        </div>
+    )
+}
