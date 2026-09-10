ecbb19276ebf7c41317f2aa60f27e3353cd459bc
Hanna Johansson <hannajohansson16@hotmail.com>
2026-09-10 23:07:35 +0200

    Add technical spec panel to overlay

--- src/ui/layout/Overlay.tsx
+++ src/ui/layout/Overlay.tsx
@@ -1,11 +1,12 @@
 import { ChoiceSummary } from "../panels/ChoiceSummary";
+import { TechSpecPanel } from "../panels/TechSpecPanel";
 
 export function Overlay() {
     return (
         <div className="grid grid-cols-3 absolute inset-0 w-dvw h-dvh pointer-events-none">
             <div className="bg-amber-200 w-36 h-36"></div>
             <div className="bg-amber-500 w-14 h-14 justify-self-center"></div>
-            <div className="bg-amber-800 w-36 h-36 justify-self-end"></div>
+            <TechSpecPanel/>
             <div className="bg-amber-950 w-96 h-36 col-span-2 self-end"></div>
             <ChoiceSummary ></ChoiceSummary>
         </div>
