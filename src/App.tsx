b104e2f1ba9808a3535413d3a6d41428b1b2cbb7
Marie Färnström <fernstrm@hotmail.com>
2026-09-11 20:07:04 +0200

    Implement light/dark toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { TruckCanvas } from './components/TruckCanvas'
 import { Overlay } from './ui/layout/Overlay'
+import { useConfiguratorStore } from './store/configuratorStore'
+import { useEffect } from 'react'
 
 function App() {
+
+  const { theme } = useConfiguratorStore()
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", theme === "dark")
+  }, [theme])
 
   return (
     <div className='static'>
