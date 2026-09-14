ae713d0b22bc8ac12aa8e2c6c18e46d7b4b21970
Marie Färnström <fernstrm@hotmail.com>
2026-09-14 15:23:00 +0200

    Added new hdri files and used them with theme toggle

--- src/components/TruckCanvas.tsx
+++ src/components/TruckCanvas.tsx
@@ -4,10 +4,14 @@ import { Hotspot } from "../scene/Hotspot"
 import { hotspots } from "../scene/hotspots"
 import { CarModel } from "../scene/model/CarModel"
 import { useDragRotation } from "../hooks/useDragRotation"
+import { useConfiguratorStore } from "../store/configuratorStore"
 
 export function TruckCanvas() {
 
     const { handlePointerDown, handlePointerMove, handlePointerUp, rotation } = useDragRotation()
+
+    const { theme } = useConfiguratorStore()
+    const hdri = (theme === "light") ? "/hdri/HDRI_Day.hdr" : "/hdri/HDRI_Night.hdr"
 
     return (
         <div className="h-dvh relative" onPointerDown={handlePointerDown}
@@ -16,12 +20,12 @@ export function TruckCanvas() {
             <Canvas camera={{ position: [0, 4, 13], fov: 45 }}>
                 <ambientLight intensity={1} />
                 <directionalLight position={[5, 5, 5]} />
+                <Environment files={hdri} />
 
                 <group
                     rotation={[0, rotation, 0]}
                     position={[0, -1, 0]}
                 >
-                    <Environment files="/hdri/test_1.hdr" />
                     <CarModel />
                     {hotspots.map((hotspot) => (
                         <Hotspot
