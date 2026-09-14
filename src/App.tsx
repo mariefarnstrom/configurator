fa67617e715539193e9e9e16239c68473cde47ad
Hanna Johansson <hannajohansson16@hotmail.com>
2026-09-14 10:01:47 +0200

    Inspect GLB hierarchy and update WIP model

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,72 @@
+// import { TruckCanvas } from './components/TruckCanvas'
+// import { Overlay } from './ui/layout/Overlay'
+// import { useConfiguratorStore } from './store/configuratorStore'
+// import { useEffect } from 'react'
+
+// function App() {
+
+//   const { theme } = useConfiguratorStore()
+
+//   useEffect(() => {
+//     document.body.classList.toggle("dark", theme === "dark")
+//   }, [theme])
+
+//   return (
+//     <div className='static'>
+//       <TruckCanvas></TruckCanvas>
+//       <Overlay></Overlay>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import { TruckCanvas } from './components/TruckCanvas'
 import { Overlay } from './ui/layout/Overlay'
-import { useConfiguratorStore } from './store/configuratorStore'
+
 import { useEffect } from 'react'
+import { useGLTF } from "@react-three/drei"
+import { MODEL_URL } from './scene/model/modelContract'
+import * as THREE from "three"
 
 function App() {
 
-  const { theme } = useConfiguratorStore()
+  function InspectGlbFile() {
+    const { scene } = useGLTF(MODEL_URL)
 
-  useEffect(() => {
-    document.body.classList.toggle("dark", theme === "dark")
-  }, [theme])
+    useEffect(() =>
+        scene.traverse((child) => {
+            if (child instanceof THREE.Group) {
+                console.log(child.name, child)
+            }
+        }))
+
+    // useEffect(() =>
+    //     scene.traverse((child) => {
+    //         if (child instanceof THREE.Mesh) {
+    //             console.log(child.name, child)
+    //         }
+    //     }))
+
+      // useEffect(() =>
+      //   scene.traverse((object) => {
+      //       if (object instanceof THREE.Mesh) {
+      //           console.log(object.name, object)
+      //       }
+      //   }))
+
+    //     scene.traverse((object) => {
+    //     console.log(object.type, object.name)
+    // })
+
+    return null
+}
 
   return (
     <div className='static'>
+      <InspectGlbFile/>
       <TruckCanvas></TruckCanvas>
       <Overlay></Overlay>
 
