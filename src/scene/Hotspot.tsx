1fbdea8138984e6aa0fa512406e6ff02c3a946d2
Hanna Johansson <hannajohansson16@hotmail.com>
2026-09-15 10:21:41 +0200

    Update hotspot toggle button with icon and design

--- src/scene/Hotspot.tsx
+++ src/scene/Hotspot.tsx
@@ -2,6 +2,8 @@ import { Html } from "@react-three/drei";
 import { useConfiguratorStore } from "../store/configuratorStore";
 import type { OptionKey } from "../types/configurator";
 
+import PlusIcon from "../assets/icons/plus.svg?react";
+
 type HotspotProps = {
     position: [number, number, number];
     option: OptionKey;
@@ -11,10 +13,18 @@ type HotspotProps = {
 export function Hotspot({ position, option }: HotspotProps) {
     const setActiveOption = useConfiguratorStore((state) => state.setActiveOption);
 
+    const activeOption = useConfiguratorStore((state) => state.activeOption);
+
+    const isOpen = activeOption === option;
+
     return (
         <Html position={position}>
-            <button className="bg-red-400" onClick={() => setActiveOption(option)}>
-                +
+            <button className="bg-hotspot aspect-square w-11.5 flex justify-center items-center rounded-full" onClick={() => setActiveOption(isOpen ? null : option)}>
+                <PlusIcon
+                        className={`transition-transform duration-300 ease-out motion-reduce:transition-none ${
+                            isOpen ? "rotate-45" : "rotate-0"
+                        }`}
+                    />
             </button>
         </Html>
     );
