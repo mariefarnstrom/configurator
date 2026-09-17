4c9768408ab9f8fa69c7c1788a766a63f71aed1d
Hanna Johansson <hannajohansson16@hotmail.com>
2026-09-17 17:59:36 +0200

    Add Zustand and simplify font setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import '@fontsource-variable/manrope'
 import '@fontsource-variable/inter'
 import './index.css'
 
